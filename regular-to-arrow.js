f2edca52e261b31533d7d77e252f7a54b8c503a5
masudmolla6 <masudmolla2937@gmail.com>
2023-02-19 11:15:20 +0600

    arrow

---
+++ regular-to-arrow.js
@@ -0,0 +1,23 @@
+// function counterFunc(counter) {
+//   if (counter > 100) {
+//     counter = 0;
+//   }else {
+//     counter++;
+//   }
+  
+//   return counter;
+// }
+// conver arrow function
+
+const counterFunc = (counter) => {
+    if (counter > 100) {
+        counter == 0;
+    }
+    else {
+        counter++;
+    }
+    return counter;
+}
+
+const result = counterFunc(55);
+console.log(result);
