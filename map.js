768b09f304e5d357a6947933c4a82f7fd2676cd4
masudmolla6 <masudmolla2937@gmail.com>
2023-03-17 19:49:41 +0600

    map 2nd problem

--- map.js
+++ map.js
@@ -1,5 +1,11 @@
-const numbers=[1,2,3,5,6,7,8];
-const newArray = numbers.map(Element => {
-    return Element;
+// 1. display all element.
+const numbers = [1, 2, 3, 5, 6, 7, 8];
+const newArray = numbers.map(Element => console.log(Element));
+
+// if element is even number you have to square with tarnarery operator.
+let array = [1, 2, 3, 4, 5, 6, 7, 8,9, 10];
+
+const ifEvenSquare = array.map(Element => {
+    return Element % 2 === 0 ? Element * Element : Element;
 })
-console.log(newArray);
+console.log(ifEvenSquare);
