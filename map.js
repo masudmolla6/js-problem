8cb694aa59c18db61c3adb66b8c5d9585bd0989e
masudmolla6 <masudmolla2937@gmail.com>
2023-03-24 15:41:09 +0600

    if numbers is odd then add 4 every element

--- map.js
+++ map.js
@@ -1,6 +1,6 @@
 // 1. display all element.
 const numbers = [1, 2, 3, 5, 6, 7, 8];
-const newArray = numbers.map(Element => console.log(Element));
+// const newArray = numbers.map(Element => console.log(Element));
 
 // if element is even number you have to square with tarnarery operator.
 let array = [1, 2, 3, 4, 5, 6, 7, 8,9, 10];
@@ -8,10 +8,22 @@ let array = [1, 2, 3, 4, 5, 6, 7, 8,9, 10];
 const ifEvenSquare = array.map(Element => {
     return Element % 2 === 0 ? Element * Element : Element;
 })
-console.log(ifEvenSquare);
+// console.log(ifEvenSquare);
 
 // add 5 with every element
 const addFive = array.map(Element => {
     return Element + 5;
 })
-console.log(addFive);
+// console.log(addFive);
+
+// if numbers is odd then add 4 every element
+const checkNumbers = [2, 3, 6, 4, 7, 3, 9, 3, 6, 2, 8];
+const oddAddFourEveryElement = checkNumbers.map(Element => {
+    if (Element % 2 === 1) {
+        return Element + 4;
+    }
+    else {
+        return Element;
+    }
+});
+console.log(oddAddFourEveryElement);
