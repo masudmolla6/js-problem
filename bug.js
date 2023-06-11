8fb885e6f3f1da15a9422417cd10252abb2f5bb8
masudmolla6 <masudmolla2937@gmail.com>
2023-06-11 14:37:26 +0600

    fixed the bug

---
+++ bug.js
@@ -0,0 +1,10 @@
+// Fix the code in the code tab to pass this challenge (only syntax errors). Look at the examples below to get an idea of what the function should do.
+// function cubes(a) {
+// 	retunr a ** 3
+// }
+
+function cubes(a) {
+	return Math.pow(a, 3);
+}
+const result = cubes(3);
+console.log(result);
